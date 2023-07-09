fee1013b55455dfd2198def3eb12b2b2a143322d
Ryan Ngoon <ryan.ngoon@gmail.com>
2023-07-08 18:57:03 -0700

    fix to sequential spawns

--- Assets/Scripts/spawnManager.cs
+++ Assets/Scripts/spawnManager.cs
@@ -10,8 +10,27 @@ public class spawnManager : MonoBehaviour
     [SerializeField] GameObject parentSpawn;
     [SerializeField] GameObject[] enemies;
 
+    List<Transform> spawnLocs;
+    int index = 0;
+
     private void Start()
     {
+        spawnLocs = new List<Transform>();
+        foreach (Transform child in parentSpawn.transform)
+        {
+            spawnLocs.Add(child);
+        }
+
+        int n = spawnLocs.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = Random.Range(0, n);
+            Transform value = spawnLocs[k];
+            spawnLocs[k] = spawnLocs[n];
+            spawnLocs[n] = value;
+        }
+
         for (int i = 0; i < spawnCount; i++)
         {
             spawn();
@@ -31,7 +50,13 @@ public class spawnManager : MonoBehaviour
 
     void spawn()
     {
-        Transform t = parentSpawn.transform.GetChild(Random.Range(0, parentSpawn.transform.childCount));
-        Instantiate(enemies[Random.Range(0, enemies.Length)], t.position, Quaternion.identity);
+        //Transform t = parentSpawn.transform.GetChild(Random.Range(0, parentSpawn.transform.childCount));
+        Transform t = spawnLocs[index];
+        index++;
+        if (index >= spawnLocs.Count)
+        {
+            index = 0;
+        }
+        Instantiate(enemies[Random.Range(0, enemies.Length)], t.position+new Vector3(Random.Range(-.5f, .5f), Random.Range(-.5f, .5f), Random.Range(-.5f, .5f)), Quaternion.identity);
     }
 }
